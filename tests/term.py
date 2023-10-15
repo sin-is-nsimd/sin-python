29649866107754326f49f73e15ca19a624de6c90
Lnc <hnc@singularity.fr>
2023-10-15 14:55:14 +0200

    Add `sin.term.<grey|red|green|orange|blue|purple>` functions

---
+++ tests/term.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2021,2023 Lénaïc Bagnères, lenaicb@singularity.fr
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import sys
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append('../src')
+import sin.term  # nopep8
+
+
+def test_sin_term():
+    '''Test the `sin.term.*` functions'''
+    print(sin.term.grey('Grey text'))
+    print(sin.term.red('Red text'))
+    print(sin.term.green('Green text'))
+    print(sin.term.orange('Orange text'))
+    print(sin.term.blue('Blue text'))
+    print(sin.term.purple('Purple text'))
